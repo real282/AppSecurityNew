544c9e4150d83059f7dd3351d2efa66796d77031
Evgeny Roznin <real282@mail.ru>
2021-08-27 16:26:15 +0800

    НАчало веб

--- src/main/java/ru/web/controller/UserController.java
+++ src/main/java/ru/web/controller/UserController.java
@@ -4,7 +4,7 @@ package ru.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.model.User;
 import ru.service.UserService;
 
@@ -20,10 +20,27 @@ public class UserController {
     }
 
 
-    @GetMapping(value = "/users")
+
+    @GetMapping(value = "/")
     public String printCars(Model model) {
         System.out.println(userService.listUsers().toString());
         model.addAttribute("messages", userService.listUsers());
         return "users";
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+
+
+
 }
