616fdafb3c2556a58f03ed794fd069c9fe2f1fd7
Evgeny Roznin <real282@mail.ru>
2021-08-30 13:30:07 +0800

    Сдача готового

--- src/main/java/ru/web/controller/UserController.java
+++ src/main/java/ru/web/controller/UserController.java
@@ -19,21 +19,18 @@ public class UserController {
         this.userService = userService;
     }
 
-    //первая страница
     @GetMapping(value = "/")
     public String printCars(Model model) {
         model.addAttribute("messages", userService.listUsers());
         return "index";
     }
 
-    //Переход на форму создания нового пользователя
     @GetMapping(value = "/create")
     public String createUser(Model model) {
         model.addAttribute("User", new User());
         return "create";
     }
 
-    //Обработка нажатия кнопки Ок в форме регистрации нового пользователя
     @RequestMapping(value = "/add")
     public String addUser(@ModelAttribute("user") User user, Model model) {
         userService.add(user);
@@ -41,26 +38,21 @@ public class UserController {
         return "create";
     }
 
-    //Обработка ссылки Delete
     @GetMapping(value = "/delete={id}")
     public String deleteUser(@PathVariable("id") long id) {
         userService.delete(id);
         return "redirect:/";
     }
 
-    //Переход по ссылке "Edit" на форму редактирования
     @GetMapping(value = "/update={id}")
     public String update(@PathVariable("id") long id, Model model) {
         model.addAttribute("User", userService.getUserId(id));
         return "update";
     }
 
-    //Обработка нажатия кнопки Ок в форме изменения пользователя
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
     public String editUser(@ModelAttribute("user") User user) {
         userService.update(user);
         return "redirect:/";
     }
-
-
 }
